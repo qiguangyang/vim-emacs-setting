c7a428612a5fd4fb737d499197aad97939533077
Guangyang Qi <qiguangyang@users.noreply.github.com>
2016-11-18 15:43:51 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 
 # link to $HOME
 echo "# Creating link in your $HOME dir..."
-mv ~/.emacs ~/.emacs-old
-mv ~/.emacs.d ~/.emacs.d-old
+#mv ~/.emacs ~/.emacs-old
+#mv ~/.emacs.d ~/.emacs.d-old
 mv ~/.vimrc ~/.vimrc-old
 mv ~/.vim ~/.vim-old
-mv ~/.config/ranger ~/.config/ranger-old
+#mv ~/.config/ranger ~/.config/ranger-old
 
-ln -vsf $PWD/emacs/.emacs ~/.emacs
-ln -vsf $PWD/emacs/.emacs.d ~/.emacs.d
+#ln -vsf $PWD/emacs/.emacs ~/.emacs
+#ln -vsf $PWD/emacs/.emacs.d ~/.emacs.d
 ln -vsf $PWD/vim/.vimrc ~/.vimrc
 ln -vsf $PWD/vim/.vim ~/.vim
 
-ln -vsf $PWD/.zshrc ~/.zshrc
-ln -vsf $PWD/.tmux.conf ~/.tmux.conf
-ln -vsf $PWD/.teamocil ~/.teamocil
-ln -vsf $PWD/ranger ~/.config
+#ln -vsf $PWD/.zshrc ~/.zshrc
+#ln -vsf $PWD/.tmux.conf ~/.tmux.conf
+#ln -vsf $PWD/.teamocil ~/.teamocil
+#ln -vsf $PWD/ranger ~/.config
 
 
 # pull submodule
-echo "# Pulling submodule"
-git submodule init
-git submodule update
+#echo "# Pulling submodule"
+#git submodule init
+#git submodule update
 
 # # auto-complete submdule
 # echo "# Pulling submodule of auto-complete"
